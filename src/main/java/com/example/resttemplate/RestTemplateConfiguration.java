1c296094a3a365135be46b5965011738b5476d0d
Tim <tim@jtim.nl>
2019-07-02 21:08:56 +0200

    Add configuration for rest template

---
+++ src/main/java/com/example/resttemplate/RestTemplateConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.resttemplate;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+//    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+//    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+}
